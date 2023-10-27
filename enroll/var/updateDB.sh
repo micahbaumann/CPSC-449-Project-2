baf941941a969f59b041c38cda61afc017ecd93f
dharbo <harboyandavid@gmail.com>
2023-10-26 17:42:16 -0700

    Fixed users.sql errors and scripts for db creation

--- enroll/var/updateDB.sh
+++ enroll/var/updateDB.sh
@@ -1,6 +1,6 @@
 # Use this file to remove the current catalog.db file and populate it with the newest version found in catalog.sql
 
 echo "Updating database file"
-rm catalog.db
-sqlite3 catalog.db -init catalog.sql
+rm ./enroll/var/catalog.db
+sqlite3 ./enroll/var/catalog.db < ./enroll/var/catalog.sql
 echo "Database file updated :)"
